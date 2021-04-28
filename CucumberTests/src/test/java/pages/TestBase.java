c3336be45e94d914f27403a98f90d91e55ea6332
marcelky <31980912+marcelky@users.noreply.github.com>
2021-04-28 18:17:37 -0300

    Use WebDriverManager

--- CucumberTests/src/test/java/pages/TestBase.java
+++ CucumberTests/src/test/java/pages/TestBase.java
@@ -3,21 +3,22 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestBase {  
 public WebDriver driver;  
 
 	public void initializeWebDriver() throws IOException {  
-		System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");  
-		driver = new ChromeDriver();  
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriverManager.chromedriver().driverVersion("90.0.4430.93").setup();
+		driver = new ChromeDriver(chromeOptions);
+		
 
-		// To maximize browser  
 		driver.manage().window().maximize();  
 
-		// Implicit wait  
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); 
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 
-		// To open browse question 
 		driver.get("https://opentdb.com/browse.php");
 
 	}  
